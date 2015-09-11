da1a7e070033d646a05e46cf283a574e00b3a76d
Maxwell Weru <mburumaxwell@gmail.com>
2015-09-11 16:06:33 +0300

    added reference to the shared project

--- AspNet5WebJobTest.WebJob/Program.cs
+++ AspNet5WebJobTest.WebJob/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace AspNet5WebJobTest.WebJob {
     public class Program {
         public void Main(string[] args) {
-            var host = new JobHost(new JobHostConfiguration("PutAValidConnectionStringHere"));
+            var host = new JobHost(new JobHostConfiguration("DefaultEndpointsProtocol=https;AccountName=th2g;AccountKey=fxtfLwgXyT1ZCokLmUWhLgEIofxmSXQGGIgHxq8pH0stVLq+WqjPZgr8JjXzSdKrfoGS7r4aS8JhD0SJN4Fv5w=="));
             host.RunAndBlock();
         }
     }
