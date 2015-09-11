872509165c0dee85d3b51fc363bc61133eaa03e7
Maxwell Weru <mburumaxwell@gmail.com>
2015-09-11 16:01:32 +0300

    configured webjob settings

---
+++ AspNet5WebJobTest.WebJob/Functions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet5WebJobTest.WebJob {
+    public class Functions {
+        // This function will get triggered/executed when a new message is written on an Azure Queue called testqueue
+        public static void ProcessHardwareReport([QueueTrigger("testqueue")] string message, TextWriter log) {
+            log.WriteLine(message);
+        }
+    }
+}
--- AspNet5WebJobTest.WebJob/Program.cs
+++ AspNet5WebJobTest.WebJob/Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using Microsoft.Azure.WebJobs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AspNet5WebJobTest.WebJob
-{
-    public class Program
-    {
-        public void Main(string[] args)
-        {
+namespace AspNet5WebJobTest.WebJob {
+    public class Program {
+        public void Main(string[] args) {
+            var host = new JobHost(new JobHostConfiguration("ConnectionStringHere"));
+            host.RunAndBlock();
         }
     }
 }
